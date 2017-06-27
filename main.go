c889093c038aead7fa4f4fdf75ec597852873ace
MeteorKL <1051814353@qq.com>
2017-06-28 07:57:03 +0800

    尝试画界面

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/MeteorKL/koala"
@@ -29,12 +30,22 @@ func main() {
 		}
 	}()
 
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/template/", http.StripPrefix("/template/", http.FileServer(http.Dir("template"))))
 	// r := koala.GetRequest(URL)
 	// ioutil.WriteFile("test.html", r, 0666)
 	// ExampleScrape(URL)
 	// koala.GetRequest(URL)
 	// url and the target path
+
 	userAPI.UserHandlers()
 	feedAPI.FeedHandlers()
+
+	koala.RenderPath = "template/"
+	koala.Get("/", func(p *koala.Params, w http.ResponseWriter, r *http.Request) {
+		koala.Render(w, "index.html", nil)
+	})
+
 	koala.RunWithLog("1123")
 }
